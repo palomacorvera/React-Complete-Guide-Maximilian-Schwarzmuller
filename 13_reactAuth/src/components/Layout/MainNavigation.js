317b9045c9552159163afae33575d6b937b385b7
Paloma Corvera <77248450+palomacorvera@users.noreply.github.com>
2021-09-28 19:31:42 +0200

    Add files via upload

---
+++ 13_reactAuth/src/components/Layout/MainNavigation.js
@@ -0,0 +1,45 @@
+import React, {useContext} from 'react';
+
+import { Link } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedin = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  }
+
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isLoggedin && (
+            <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          )}
+          {isLoggedin && (
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          )}
+          {isLoggedin && (
+          <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
